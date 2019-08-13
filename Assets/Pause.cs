cecfc47865a9afd1c14e7475fc7a3322ee84b949
Nathan Burton <Nathan.Burton@mail.bcu.ac.uk>
2019-08-13 14:55:16 +0100

    Pause Screen

---
+++ Assets/Pause.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject raceHandler;
+    public SpriteRenderer pauseRender;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseRender = GetComponent<SpriteRenderer>();
+        pauseRender.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (raceHandler.GetComponent<RaceHandler>().paused == true)
+        {
+            pauseRender.enabled = true;
+        }
+        if (raceHandler.GetComponent<RaceHandler>().paused == false)
+        {
+            pauseRender.enabled = false;
+        }
+    }
+}
